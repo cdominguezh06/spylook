e017b985e15a4ac94c988695c93aaecce57ea388
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-13 12:14:13 +0200

    Actualizar app según release más reciente en GitHub

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
