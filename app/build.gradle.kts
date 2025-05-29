611946ca1ee58932414241efe3d5e6b699f3750e
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-29 18:28:27 +0200

    - Solucion de error que impedía actualizar después de aceptar los permisos de instalaciones de origenes desconocidos por primera vez

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "0.3.1"
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
