d1b9c27cbe83a87e7173a1ee3198bedf7a4ba653
cdominguezh06 <156293143+cdominguezh06@users.noreply.github.com>
2025-05-31 20:43:00 +0200

    - Cambios en README.md - Mejora de funcionamiento de DateTextWatcher.kt para campos de fecha

--- app/src/main/java/com/cogu/spylook/model/utils/textWatchers/DateTextWatcher.kt
+++ app/src/main/java/com/cogu/spylook/model/utils/textWatchers/DateTextWatcher.kt
@@ -12,48 +12,58 @@ import kotlin.math.min
 class DateTextWatcher(private val editText: EditText) : TextWatcher {
     private var current = ""
     private val ddmmyyyy = "DDMMYYYY"
-    private val cal: Calendar = Calendar.getInstance()
+    private val calendario: Calendar = Calendar.getInstance()
+    private var lastNumberIndex = 0
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         if (s.toString() != current) {
-            var clean = s.toString().replace("[^\\d]".toRegex(), "")
-            val cleanC = current.replace("[^\\d]".toRegex(), "")
-
-            val cl = clean.length
-            var sel = cl
+            var clean = s.toString().replace("\\D".toRegex(), "")
+            val cleanCurrent = current.replace("\\D".toRegex(), "")
+            val cleanLength = clean.length
+            var selection = cleanLength
             var i = 2
-            while (i <= cl && i < 6) {
-                sel++
+
+            while (i <= cleanLength && i < 6) {
+                selection++
                 i += 2
             }
-            if (clean == cleanC) sel--
+
+            if (clean == cleanCurrent) selection--
 
             if (clean.length < 8) {
                 clean = clean + ddmmyyyy.substring(clean.length)
             } else {
-                var day = clean.substring(0, 2).toInt()
-                var mon = clean.substring(2, 4).toInt()
-                var year = clean.substring(4, 8).toInt()
+                var dia = clean.substring(0, 2).toInt()
+                var mes = clean.substring(2, 4).toInt()
+                var anno = clean.substring(4, 8).toInt()
 
-                year = max(1970.0, min(year.toDouble(), LocalDate.now().year.toDouble())).toInt()
-                if (year == LocalDate.now().year) {
-                    mon = max(1.0, min(mon.toDouble(), LocalDate.now().monthValue.toDouble())).toInt()
+                anno = max(1970.0, min(anno.toDouble(), LocalDate.now().year.toDouble())).toInt()
+                if (anno == LocalDate.now().year) {
+                    mes =
+                        max(1.0, min(mes.toDouble(), LocalDate.now().monthValue.toDouble())).toInt()
                 } else {
-                    mon = max(1.0, min(mon.toDouble(), 12.0)).toInt()
+                    mes = max(1.0, min(mes.toDouble(), 12.0)).toInt()
                 }
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, mon - 1)
-                if (mon == LocalDate.now().monthValue && year == LocalDate.now().year) {
-                    day = max(1.0, min(day.toDouble(), LocalDate.now().dayOfMonth.toDouble())).toInt()
-                }else{
-                    day = max(
+
+                calendario.set(Calendar.YEAR, anno)
+                calendario.set(Calendar.MONTH, mes - 1)
+
+                if (mes == LocalDate.now().monthValue && anno == LocalDate.now().year) {
+                    dia =
+                        max(1.0, min(dia.toDouble(), LocalDate.now().dayOfMonth.toDouble())).toInt()
+                } else {
+                    dia = max(
                         1.0,
-                        min(day.toDouble(), cal.getActualMaximum(Calendar.DAY_OF_MONTH).toDouble())
+                        min(
+                            dia.toDouble(),
+                            calendario.getActualMaximum(Calendar.DAY_OF_MONTH).toDouble()
+                        )
                     ).toInt()
                 }
-                clean = String.format(Locale.getDefault(), "%02d%02d%04d", day, mon, year)
+
+                clean = String.format(Locale.getDefault(), "%02d%02d%04d", dia, mes, anno)
             }
 
             clean = String.format(
@@ -64,7 +74,19 @@ class DateTextWatcher(private val editText: EditText) : TextWatcher {
 
             current = clean
             editText.setText(current)
-            editText.setSelection(min(sel.toDouble(), current.length.toDouble()).toInt())
+
+            if (count > 0 && before == 0) {
+                if(start > lastNumberIndex){
+                    editText.setSelection(min(selection.toDouble(), current.length.toDouble()).toInt())
+                }else
+                    editText.setSelection(start+count)
+            }else{
+                editText.setSelection(start)
+            }
+
+            lastNumberIndex = s.toString().lastIndexOf(
+                current.replace("\\D".toRegex(), "")[current.replace("\\D".toRegex(), "").length - 1])
+
         }
     }
 
